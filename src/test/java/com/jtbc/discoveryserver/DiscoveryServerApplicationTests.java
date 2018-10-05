864518bf7f57646aec2ec69938dd683c100f2d37
Jeff Trimmer <trimmerjeff@gmail.com>
2018-10-05 07:15:21 -0700

    initial commit

---
+++ src/test/java/com/jtbc/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jtbc.discoveryserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryServerApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
